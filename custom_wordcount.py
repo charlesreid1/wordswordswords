89f8f321305697023c471693afc53670e5818be4
Charles Reid <charlesreid1@gmail.com>
2014-12-21 05:10:27 -0800

    updating custom wordcount, to compute histogram of word lengths.

--- custom_wordcount.py
+++ custom_wordcount.py
@@ -1,23 +1,25 @@
 from __future__ import division
+import re
 import nltk
 from nltk.tokenize.punkt import PunktWordTokenizer
 from nltk.text import Text
+import matplotlib.pylab as plt
 
-filenames=['data/crimeandpunishment.txt',
-           'data/brotherskaramazov.txt',
-           'data/theidiot.txt']
+filenames=['crimeandpunishment.txt',
+           'brotherskaramazov.txt',
+           'theidiot.txt']
 
 for filename in filenames:
 
-    with open(filename) as f:
+    with open('data/'+filename) as f:
 
         print "======================================"
         print filename 
         print "======================================"
 
         bk = f.read()
-        bk = bk.replace('\r','')
-        bk = bk.replace('\n','')
+        bk = bk.replace('\r',' ')
+        bk = bk.replace('\n',' ')
 
         tok = PunktWordTokenizer().tokenize(bk)
 
@@ -33,13 +35,26 @@ for filename in filenames:
         print "\n"+"-"*40+"\n\n"
 
         fdist = nltk.FreqDist(tok)
-        mc = fdist.most_common(100)
+        mc = fdist.most_common(10)
         for ww in mc:
             print "%s\t\t%d"%(ww)#word,wordfreq)
 
-        fdist.plot(50, cumulative=True)
+        ### fdist.plot(50, cumulative=True)
 
-        #import pdb; pdb.set_trace()
-        #a=0
+        z = [len(j) for j in tok]
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+
+        ax1.hist(z,40,color='g',bottom=0.001)
+
+        ax1.set_title(filename)
+        ax1.set_yscale('log')
+
+        figname = filename[:-4]+'.jpg'
+        print figname
+
+        plt.show()
+        plt.draw()
+        fig.savefig('img/'+figname)
 
         print "\n"*2
