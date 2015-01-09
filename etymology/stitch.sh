1c29489e1fa4c4795446dc50cab14cf72e7724e2
Charles Reid <charlesreid1@gmail.com>
2015-01-09 14:08:31 -0800

    small dubliners updates. something is not quite working right with the OED scraping. dutch words showing up as greek, him showing up as arabic, etc.

--- etymology/stitch.sh
+++ etymology/stitch.sh
@@ -7,6 +7,11 @@ cat key.html              >> $DEST/copperfield.html
 cat copperfield_body.html >> $DEST/copperfield.html
 cat foot.html             >> $DEST/copperfield.html
 
+cat head.html              > $DEST/dubliners.html
+cat key.html              >> $DEST/dubliners.html
+cat dubliners_body.html   >> $DEST/dubliners.html
+cat foot.html             >> $DEST/dubliners.html
+
 #cat head.html           > $DEST/small.html
 #cat key.html           >> $DEST/small.html
 #cat small_body.html    >> $DEST/small.html
