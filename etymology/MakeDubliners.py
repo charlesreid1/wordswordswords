ecdd8e13fe07abd53a02b31a39b7cc336dd6fab5
Charles Reid <charlesreid1@gmail.com>
2015-01-16 18:22:22 -0800

    updating dubliners html and scripts.

---
+++ etymology/MakeDubliners.py
@@ -0,0 +1,16 @@
+#from EtymologyCSV import *
+#c = EtymologyCSV('copperfield')
+#c.export_csv()
+
+#from EtymologyCSV import *
+#c = EtymologyCSV('dubliners')
+##c.export_word_file()
+#c.export_rootlang_file()
+
+#from EtymologyHTML import *
+#h = EtymologyHTML('copperfield')
+#h.export_html_file()
+
+from EtymologyChapterHTML import *
+h = EtymologyChapterHTML('dubliners')
+h.export_html_file()
