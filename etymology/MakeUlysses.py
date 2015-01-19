de0f0318e4e70e9e3f2b31a4a134983e47dfc06d
Charles Reid <charlesreid1@gmail.com>
2015-01-18 20:03:10 -0800

    adding ulysses to etymology scripts/output

---
+++ etymology/MakeUlysses.py
@@ -0,0 +1,8 @@
+#from EtymologyCSV import *
+#c = EtymologyCSV('ulysses')
+#c.export_word_file()
+#c.export_rootlang_file()
+
+from EtymologyChapterHTML import *
+h = EtymologyChapterHTML('ulysses')
+h.export_html_file()
